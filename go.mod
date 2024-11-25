740b1f7fcf09e5d4755191b30e2ec56267c65311
pehringer <pehringer.jacob@gmail.com>
2024-11-24 23:43:38 -0700

    network forward prop

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/pehringer/gobed
 go 1.23.1
 
 require (
-	github.com/pehringer/simd v1.1.3
+	github.com/pehringer/simd v1.1.3 // indirect
 	golang.org/x/sys v0.27.0 // indirect
 )
--- internal/vec/vec.go
+++ internal/vec/vec.go
@@ -17,28 +17,51 @@ func Add(a, b, c []float32) {
 	simd.AddFloat32(a, b, c)
 }
 
-func Dot(a, b []float32, c *float32) {
-	n := min(len(a), len(b))
-	d := make([]float32, n)
-	simd.MulFloat32(a, b, d)
-	i := n
-	j := n / 2
+
+func Sum(a []float32) {
+	i := len(a)
+	j := len(a) / 2
 	for j != 0 {
-		simd.AddFloat32(d[0:j], d[j:i], d[0:j])
+		simd.AddFloat32(a[0:j], a[j:i], a[0:j])
 		if i%2 == 1 {
-			d[0] += d[i-1]
+			a[0] += a[i-1]
 		}
 		i = j
 		j /= 2
 	}
+}
+
+func Dot(a, b []float32, c *float32) {
+	n := min(len(a), len(b))
+	d := make([]float32, n)
+	simd.MulFloat32(a, b, d)
+	Sum(d)
 	*c = d[0]
 }
 
 func ReLU(a, b []float32) {
 	n := min(len(a), len(b))
 	for i := 0; i < n; i++ {
-		b[i] = max(0.0, a[i])
+		b[i] = float32(math.Max(0.0, float64(a[i])))
 	}
+}
+
+func Softmax(a, b []float32) {
+    n := min(len(a), len(b))
+    m := a[0]
+    for i := 0; i < n; i++ {
+        if a[i] > m {
+            m = a[i]
+        }
+    }
+    s := float32(0)
+    for i := 0; i < n; i++ {
+        b[i] = float32(math.Exp(float64(a[i] - m)))
+        s += b[i]
+    }
+    for i := 0; i < n; i++ {
+        b[i] /= s
+    }
 }
 
 func Sub(a, b, c []float32) {
